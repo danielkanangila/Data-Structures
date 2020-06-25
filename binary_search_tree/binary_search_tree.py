ab8fac4debbea94170144391029cad65f78ae773
danielkanangila <daniel.kanangila@gmail.com>
2020-06-24 22:52:02 -0400

    cleanup

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -135,15 +135,3 @@ class BSTNode:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
-
-
-bst = BSTNode(1)
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(4)
-bst.insert(2)
-
-bst.dft_print(bst)
